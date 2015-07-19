7f276609f611e42b3cb90fea6a202bf40d968ea2
Yunchen Wei <weiyunchen0807@outlook.com>
2015-07-18 22:31:01 -0400

    Refactor template HTML

--- run.py
+++ run.py
@@ -8,11 +8,11 @@ def home():
 
 @app.route('/map')
 def map():
-  return render_template('index.html')
+  return render_template('map.html')
 
-@app.route('/about')
-def about():
-  return render_template('index.html')
+# @app.route('/about')
+# def about():
+#   return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
