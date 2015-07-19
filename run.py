55aa16541a8ea780899004e8d32da84211ec47ec
Yunchen Wei <weiyunchen0807@outlook.com>
2015-07-19 00:52:34 -0400

    Add Video in index page

--- run.py
+++ run.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/map')
 def map():
-  return render_template('map.html')
+    return render_template('map.html')
+
+
+@app.route('/test')
+def test():
+    return jsonify({"a": 1})
 
 # @app.route('/about')
 # def about():
 #   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
